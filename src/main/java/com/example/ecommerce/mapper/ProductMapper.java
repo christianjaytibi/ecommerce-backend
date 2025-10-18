fe3c570fa33d5da1c8ba1378d3db35f701b64f1b
Christian Jay Tibi <christianjaytibi@gmail.com>
2025-10-18 21:13:42 +0800

    add mapping for product DTO to entity conversion

--- src/main/java/com/example/ecommerce/mapper/ProductMapper.java
+++ src/main/java/com/example/ecommerce/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.mapper;
 
+import com.example.ecommerce.dto.ProductRequestDto;
 import com.example.ecommerce.dto.ProductResponseDto;
 import com.example.ecommerce.entity.Product;
 import org.mapstruct.Mapper;
@@ -8,4 +9,5 @@ import org.mapstruct.Mapper;
 public interface ProductMapper {
 
      ProductResponseDto toDto (Product product);
+     Product toEntity(ProductRequestDto productDto);
 }
