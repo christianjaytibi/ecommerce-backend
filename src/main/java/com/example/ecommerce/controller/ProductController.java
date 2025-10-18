4abe577211aded2abbc720eeae3b0e42cc737e80
Christian Jay Tibi <christianjaytibi@gmail.com>
2025-10-18 22:08:31 +0800

    feat: implement endpoint for creating products

--- src/main/java/com/example/ecommerce/controller/ProductController.java
+++ src/main/java/com/example/ecommerce/controller/ProductController.java
@@ -1,13 +1,19 @@
 package com.example.ecommerce.controller;
 
+import com.example.ecommerce.dto.ProductRequestDto;
 import com.example.ecommerce.dto.ProductResponseDto;
 import com.example.ecommerce.service.ProductService;
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
@@ -29,5 +35,16 @@ public class ProductController {
     @GetMapping("/{id}")
     ResponseEntity<ProductResponseDto> getProductDetails(@PathVariable UUID id) {
         return ResponseEntity.ok(service.getProductDetails(id));
+    }
+
+    @PostMapping
+    ResponseEntity<ProductResponseDto> createProduct(@Valid @RequestBody ProductRequestDto request) {
+        ProductResponseDto productDto = service.createProduct(request);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(productDto.getId())
+                .toUri();
+        return ResponseEntity.created(location).body(productDto);
     }
 }
