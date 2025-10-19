c1603189da6b5e5e1119dfff7b1e88aa45287e07
Christian Jay Tibi <christianjaytibi@gmail.com>
2025-10-19 22:18:03 +0800

    feat: add mapper method for updating product entities using request DTOs

--- src/main/java/com/example/ecommerce/mapper/ProductMapper.java
+++ src/main/java/com/example/ecommerce/mapper/ProductMapper.java
@@ -4,10 +4,12 @@ import com.example.ecommerce.dto.ProductRequestDto;
 import com.example.ecommerce.dto.ProductResponseDto;
 import com.example.ecommerce.entity.Product;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
 
      ProductResponseDto toDto (Product product);
      Product toEntity(ProductRequestDto productDto);
+     void updateProduct(@MappingTarget Product product, ProductRequestDto productDto);
 }
